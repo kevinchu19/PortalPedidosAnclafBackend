254c1ae710b7ee725d1cccf18c5187870e3c659b
Kevin Tchubinsky <kevinchu19@gmail.com>
2021-04-30 17:36:34 -0300

    Se aplica timer en background service para informar pedidos

--- PortalPedidosAnclafBackend/Services/ConsumeScopedServiceHostedService.cs
+++ PortalPedidosAnclafBackend/Services/ConsumeScopedServiceHostedService.cs
@@ -12,6 +12,7 @@ namespace PortalPedidosAnclafBackend.Services
     public class ConsumeScopedServiceHostedService : BackgroundService
     {
         private readonly ILogger _logger;
+        private Timer _timer { get; set; }
 
         public ConsumeScopedServiceHostedService(IServiceScopeFactory services,
             ILogger logger)
@@ -21,24 +22,30 @@ namespace PortalPedidosAnclafBackend.Services
         }
 
         public IServiceScopeFactory Services { get; }
+        
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             _logger.Information(
                 "Consume Scoped Service Hosted Service running.");
+
+            
+                _logger.Information(
+                    $"Scoped Processing Service is working. Count: {0}");
+
             try
             {
-                await DoWork(stoppingToken);
+                _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
             }
             catch (Exception ex)
             {
 
                 _logger.Fatal($"Error al ejecutar el servicio {ex.Message}");
             }
-            
+
         }
 
-        private async Task DoWork(CancellationToken stoppingToken)
+        private async void DoWork(object state)
         {
             _logger.Information(
                 "Consume Scoped Service Hosted Service is working.");
@@ -49,7 +56,7 @@ namespace PortalPedidosAnclafBackend.Services
                     scope.ServiceProvider
                         .GetRequiredService<IScopedProcessingService>();
 
-                await scopedProcessingService.DoWork(stoppingToken);
+                await scopedProcessingService.DoWork();
             }
         }
 
--- PortalPedidosAnclafBackend/Services/PostearPedidoEnSoftlandService.cs
+++ PortalPedidosAnclafBackend/Services/PostearPedidoEnSoftlandService.cs
@@ -18,7 +18,7 @@ namespace PortalPedidosAnclafBackend.Services
 {
     internal interface IScopedProcessingService
     {
-        Task DoWork(CancellationToken stoppingToken);
+        Task DoWork();
     }
 
     internal class PostearPedidoEnSoftlandService : IScopedProcessingService
@@ -38,55 +38,49 @@ namespace PortalPedidosAnclafBackend.Services
             _configuration = configuration;
         }
 
-        public async Task DoWork(CancellationToken stoppingToken)
+        public async Task DoWork()
         {
-            while (!stoppingToken.IsCancellationRequested)
+
+            IEnumerable<PedidoDTO> pedidos = _mapper.Map<IEnumerable<Pedido>,IEnumerable<PedidoDTO>>(await _repository.Pedidos.GetForSoftland(0, Convert.ToInt32(_configuration["HostSoftland:CantidadPedidosPorProceso"])));
+
+            if (pedidos.Count() == 0)
             {
-                executionCount++;
-                _logger.Information(
-                    $"Scoped Processing Service is working. Count: {0}", executionCount);
-
-                IEnumerable<PedidoDTO> pedidos = _mapper.Map<IEnumerable<Pedido>,IEnumerable<PedidoDTO>>(await _repository.Pedidos.GetForSoftland(0, Convert.ToInt32(_configuration["HostSoftland:CantidadPedidosPorProceso"])));
-
-                if (pedidos.Count() == 0)
-                {
-                    return;
-                }
-                string pedidosString = JsonSerializer.Serialize(pedidos, new JsonSerializerOptions { WriteIndented = true });
-
-                _logger.Information($"{ pedidosString }");
-                HttpResponseMessage stringTask = await client.PostAsync($"{_configuration["HostSoftland:BasePath"]}/api/pedido", new StringContent(pedidosString, Encoding.UTF8, "application/json"));
-                var stream = await stringTask.Content.ReadAsStreamAsync();
-                var content = await JsonSerializer.DeserializeAsync<ApiSoftlandResponse[]>(stream);
-
-
-                foreach ((PedidoDTO pedido, Int32 i) in pedidos.Select((pedido, i) => (pedido, i)))
-                {
-                    if (content[i].estado == 200)
-                    {
-                        try 
-                        {
-                            await _repository.Pedidos.ActualizaPedidoTransferido(pedido.Id, 1);
-                            await _repository.Complete();
-                        }
-                        catch (Exception ex)
-                        {
-                            var a = ex.Message;
-                            
-                        }
-                        
-                    }
-
-                    else
-                    {
-                        await _repository.Pedidos.ActualizaPedidoTransferido(pedido.Id, 9);
-                        await _repository.Complete();
-                        _logger.Error($"({content[i].estado}) Error al procesar pedido {pedido.Id}: { content[i].mensaje }");
-                    }
-
-                }
-                await Task.Delay(10000, stoppingToken);
+                return;
             }
+            string pedidosString = JsonSerializer.Serialize(pedidos, new JsonSerializerOptions { WriteIndented = true });
+
+            _logger.Information($"{ pedidosString }");
+            HttpResponseMessage stringTask = await client.PostAsync($"{_configuration["HostSoftland:BasePath"]}/api/pedido", new StringContent(pedidosString, Encoding.UTF8, "application/json"));
+            var stream = await stringTask.Content.ReadAsStreamAsync();
+            var content = await JsonSerializer.DeserializeAsync<ApiSoftlandResponse[]>(stream);
+
+
+            foreach ((PedidoDTO pedido, Int32 i) in pedidos.Select((pedido, i) => (pedido, i)))
+            {
+                if (content[i].estado == 200)
+                {
+                    try 
+                    {
+                        await _repository.Pedidos.ActualizaPedidoTransferido(pedido.Id, 1);
+                        await _repository.Complete();
+                    }
+                    catch (Exception ex)
+                    {
+                        var a = ex.Message;
+                            
+                    }
+                        
+                }
+
+                else
+                {
+                    await _repository.Pedidos.ActualizaPedidoTransferido(pedido.Id, 9);
+                    await _repository.Complete();
+                    _logger.Error($"({content[i].estado}) Error al procesar pedido {pedido.Id}: { content[i].mensaje }");
+                }
+
+            }
+            
         }
     }
 }
