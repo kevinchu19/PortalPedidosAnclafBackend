0c190a79d152c826459c8f2a9cebb9c4a1c1f761
Kevin Tchubinsky <kevinchu19@gmail.com>
2021-05-21 13:11:18 -0300

    Se cambia propiedad de naming policy

--- PortalPedidosAnclafBackend/Startup.cs
+++ PortalPedidosAnclafBackend/Startup.cs
@@ -40,6 +40,12 @@ namespace PortalPedidosAnclafBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddMvc(Options =>{})
+                .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+
             services.AddHostedService<ConsumeScopedServiceHostedService>();
             services.AddScoped<IScopedProcessingService, PostearPedidoEnSoftlandService>();
 
