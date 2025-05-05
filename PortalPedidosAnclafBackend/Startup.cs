985532e89fdcc22bc9b3dea708810e77eaadaa17
Kevin Tchubinsky <kevinchu19@gmail.com>
2025-05-05 17:11:23 -0300

    Se agrega ssl mode=none

--- PortalPedidosAnclafBackend/Startup.cs
+++ PortalPedidosAnclafBackend/Startup.cs
@@ -70,7 +70,7 @@ namespace PortalPedidosAnclafBackend
             
             services.AddSingleton<Serilog.ILogger>(options =>
             {
-                var connstring = "Server=localhost;Port=3306;User=alamo_pedidos;Password=eUWCX8idrrEuVTcuia0=;Database=portalpedidosanclaflex";//Configuration["Serilog:SerilogConnectionString"];
+                var connstring = "Server=localhost;Port=3306;User=alamo_pedidos;Password=eUWCX8idrrEuVTcuia0=;Database=portalpedidosanclaflex;SSL Mode=None";//Configuration["Serilog:SerilogConnectionString"];
                 var tableName = "APILogs";//Configuration["Serilog:TableName"];
 
                 return new LoggerConfiguration()
@@ -129,7 +129,7 @@ namespace PortalPedidosAnclafBackend
                     maxRetryDelay: System.TimeSpan.FromSeconds(20),
                     errorNumbersToAdd: null)
                 )
-               .EnableSensitiveDataLogging()
+             
                 
                 
                );
